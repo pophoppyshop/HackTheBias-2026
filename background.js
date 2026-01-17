51dd5a63093db8a6ab05fa7e7706be32e0c076b5
Li Tan <robloxcity88@gmail.com>
2026-01-17 16:28:10 -0700

    Current tab information can be read.

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['content.js'] // Injects and runs the content.js file
-  });
-});
--- hello.js
+++ hello.js
@@ -1,19 +1,3 @@
-async function getCurrentTab() {
-    // active: filters for the current viewed tab
-    // lastFocusedWindow: restrict to only the most recent focued window
-    let queryOptions = { active: true, lastFocusedWindow: true };
-
-    // `tabs` is an array of tabs that match the query
-    let [tab] = await chrome.tabs.query(queryOptions); 
-    return tab;
-}
-
-// Example usage:
-getCurrentTab().then(tab => {
-  console.log("Current Tab URL:", tab.url);
-  console.log("Current Tab Title:", tab.title);
-});
-
 // Query for the active tab in the current window
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     const tabId = tabs[0].id;
@@ -25,14 +9,19 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     }, () => {
         // After injection, send the message
         chrome.tabs.sendMessage(tabId, { action: "getPageContent" }, function(response) {
+            // Send error if content can't be extracted
             if (chrome.runtime.lastError) {
                 console.error("Error sending message:", chrome.runtime.lastError);
+
                 document.getElementById('contentArea').innerText = 'Error: Could not retrieve page content.';
+
                 return;
             }
 
+            // Show content snippet if received
             if (response && response.content) {
                 console.log("Page content:", response.content);
+
                 // Display the content in the popup
                 document.getElementById('contentArea').innerText = response.content.substring(0, 500) + '...';
             } else {

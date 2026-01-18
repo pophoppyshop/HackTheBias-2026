91e22671668b2389be6413f711a396cb80d68925
Li Tan <robloxcity88@gmail.com>
2026-01-17 19:16:08 -0700

    Worked on expand button

--- hello.js
+++ hello.js
@@ -1,3 +1,27 @@
+async function postData(url, data) {
+  try {
+    // Pause until a response is received
+    const response = await fetch(url, {
+      method: 'POST', // Specify the method
+      headers: {
+        'Content-Type': 'application/json' // Inform the server the body format
+      },
+      body: JSON.stringify(data) // Convert the JavaScript object to a JSON string
+    });
+
+    // Check for HTTP errors
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const responseData = await response.json(); // Parse the response to a usuable JavaScript object
+    console.log('Success:', responseData);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+
 // Query for the active tab in the current window
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     const tabId = tabs[0].id;
@@ -21,6 +45,16 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
 
             // Show content snippet if received
             if (response && response.content) {
+                // Prepare url and data for POST request
+                const apiEndpoint = 'https://hackthebias-2026.onrender.com/items/';
+                const dataToSend = {
+                    title: response.title,
+                    content: response.content
+                }
+
+                // Post data to API
+                postData(apiEndpoint, dataToSend);
+
                 console.log("Page content:", response);
 
                 // Display the extracted article information
@@ -34,4 +68,9 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
             }
         });
     });
+});
+
+// Add event listener for expand button
+document.getElementById('expandBtn').addEventListener('click', () => {
+    document.body.classList.toggle('expanded');
 });
